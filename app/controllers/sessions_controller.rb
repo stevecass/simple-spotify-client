f34dfdf4ffab5488a2a671d67a1ed6a0f38b0488
Steven Cassidy <steven.cassidy@gmail.com>
2015-11-17 20:53:46 -0500

    Implement spotify oauth login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,13 @@
 class SessionsController < ApplicationController
   def create
-    p request.env
-    p "XXXXXXXXXXXXX"
-    p request
-    render text: 'OK'
+    auth_hash = request.env['omniauth.auth']
+    u = User.find_or_create_by uid: auth_hash[:uid], provider: auth_hash[:provider]
+    u.email = auth_hash[:info][:email]
+    u.name  = auth_hash[:info][:name]
+    u.image = auth_hash[:info][:image]
+    u.save if u.changed?
+    session[:user_id] = u.id
+    redirect_to root_path
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'auth/:provider/callback' => 'sessions#create', as: :ouath_callback
+  root 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
